6e59a989068ba8d73725112abde37ede2eb75c13
rajaram <rajaram@runloyal.com>
2023-04-28 17:07:27 +0530

    aprilseven

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -26,39 +26,37 @@ public class F1 extends Car {
 
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
-            changeGear(1);
+            super.changeGear(1);
             super.stop();
         }
         //for all other cases, change the gear accordingly
-
+        if (newSpeed > 0 && newSpeed <= 50) {
+            // Set gear as 1 for speed 1-50
+            super.changeGear(1);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        } else if (newSpeed > 50 && newSpeed <= 100) {
+            // Set gear as 2 for speed 51-100
+            super.changeGear(2);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        } else if (newSpeed > 100 && newSpeed <= 150) {
+            // Set gear as 3 for speed 101-150
+            super.changeGear(3);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        } else if (newSpeed > 150 && newSpeed <= 200) {
+            // Set gear as 4 for speed 151-200
+            super.changeGear(4);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        } else if (newSpeed > 200 && newSpeed <= 250) {
+            // Set gear as 5 for speed 201-250
+            super.changeGear(5);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        } else if (newSpeed > 250) {
+            // Set gear as 6 for speed more than 250
+            super.changeGear(6);
+//                changeSpeed(newSpeed, getCurrentDirection());
+        }
         if(newSpeed > 0) {
-            if (newSpeed > 0 && newSpeed <= 50) {
-                // Set gear as 1 for speed 1-50
-                super.changeGear(1);
-                changeSpeed(newSpeed, getCurrentDirection());
-            } else if (newSpeed > 50 && newSpeed <= 100) {
-                // Set gear as 2 for speed 51-100
-                super.changeGear(2);
-                changeSpeed(newSpeed, getCurrentDirection());
-            } else if (newSpeed > 100 && newSpeed <= 150) {
-                // Set gear as 3 for speed 101-150
-                super.changeGear(3);
-                changeSpeed(newSpeed, getCurrentDirection());
-            } else if (newSpeed > 150 && newSpeed <= 200) {
-                // Set gear as 4 for speed 151-200
-                super.changeGear(4);
-                changeSpeed(newSpeed, getCurrentDirection());
-            } else if (newSpeed > 200 && newSpeed <= 250) {
-                // Set gear as 5 for speed 201-250
-                super.changeGear(5);
-                changeSpeed(newSpeed, getCurrentDirection());
-            } else if (newSpeed > 250) {
-                // Set gear as 6 for speed more than 250
-                super.changeGear(6);
-                changeSpeed(newSpeed, getCurrentDirection());
-            }
-//            changeSpeed(newSpeed, getCurrentDirection());
-
+            changeSpeed(newSpeed, getCurrentDirection());
         }
     }
 
